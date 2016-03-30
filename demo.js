ed1705feb75e0314ed973ee8e69f17be7ccd2c67
Anand Aravindan <anand.anand84@gmail.com>
2016-03-30 11:58:06 -0500

    Publish Events when new keys is subscribed and last subscriber unsubscribes for the key

--- demo.js
+++ demo.js
@@ -1,3 +1,20 @@
 /**
  * Created by AAravindan on 3/30/16.
  */
+var PubSub  = require('./index.js');
+
+var stockPublisher = new PubSub();
+
+stockPublisher.on('entry', function(topic){
+  console.log('New topic subscribed', topic);
+})
+
+stockPublisher.on('empty', function(topic){
+  console.log('All listeners unsubscribed for topic', topic);
+})
+
+var subscriptionID = stockPublisher.subscribe('NSE:RELIANCE', function(){
+  console.log('Subscription result for ', Reliance);
+})
+
+stockPublisher.unsubscribe(subscriptionID);
--- index.js
+++ index.js
@@ -1,46 +1,53 @@
-module.exports = (function() {
-    function PubSub() {
-        "use strict";
-        this.topics = {};
-        this.subUid = -1;
-    }
+var EntryEmptyEmitter = require('entry-empty-emitter');
+const util = require('util');
 
-    PubSub.prototype.publish = function(topic, args) {
-        if (!this.topics[topic]) {
-            return false;
+function PubSub() {
+  "use strict";
+  EntryEmptyEmitter.call(this);
+  this.topics = {};
+  this.subUid = -1;
+}
+
+util.inherits(PubSub, EntryEmptyEmitter);
+
+PubSub.prototype.publish = function (topic, args) {
+  if (!this.topics[topic]) {
+    return false;
+  }
+  var subscribers = this.topics[topic], len = subscribers ? subscribers.length : 0;
+  while (len--) {
+    subscribers[len].func(topic, args);
+  }
+  return true;
+};
+PubSub.prototype.subscribe = function (topic, func) {
+  if (!this.topics[topic]) {
+    this.topics[topic] = [];
+  }
+  var token = (++this.subUid).toString();
+  this.topics[topic].push({
+    token: token,
+    func: func
+  });
+  this.add(topic);
+  return token;
+};
+PubSub.prototype.unsubscribe = function (token) {
+  for (var m in this.topics) {
+    if (this.topics[m]) {
+      for (var i = 0, j = this.topics[m].length; i < j; i++) {
+        if (this.topics[m][i].token === token) {
+          this.topics[m].splice(i, 1);
+          this.remove(m);
+          return true;
         }
-        var subscribers = this.topics[topic], len = subscribers ? subscribers.length : 0;
-        while (len--) {
-            subscribers[len].func(topic, args);
-        }
-        return true;
-    };
-    PubSub.prototype.subscribe = function(topic, func) {
-        if (!this.topics[topic]) {
-            this.topics[topic] = [];
-        }
-        var token = (++this.subUid).toString();
-        this.topics[topic].push({
-            token: token,
-            func: func
-        });
-        return token;
-    };
-    PubSub.prototype.unsubscribe = function(token) {
-        for (var m in this.topics) {
-            if (this.topics[m]) {
-                for (var i = 0, j = this.topics[m].length; i < j; i++) {
-                    if (this.topics[m][i].token === token) {
-                        this.topics[m].splice(i, 1);
-                        return true;
-                    }
-                }
-                if (this.topics[m].length == 0) {
-                    delete this.topics[m];
-                }
-            }
-        }
-        return false;
-    };
-    return new PubSub();
-})();
+      }
+      if (this.topics[m].length == 0) {
+        delete this.topics[m];
+      }
+    }
+  }
+  return false;
+};
+
+global.Pubsub = module.exports = PubSub;
