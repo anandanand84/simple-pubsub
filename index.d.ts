a3187d3bba3d8221784a34cd4a0532d5072aa811
Anand Aravindan <anand.anand84@gmail.com>
2017-03-10 14:39:15 -0600

    Fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="node" />
 import { EventEmitter } from 'events';
-declare module "simple-pubsub" {
-    export interface PubSub extends EventEmitter{
-        publish:(topic:string, data:any)=>boolean;
-        subscribe:(topic:string, callback:(topic:string)=>string)=>string;
-        unsubscribe:(token:string)=>boolean;
-    }
+declare class PubSub extends EventEmitter{
+    constructor();
+    publish:(topic:string, data:any)=>boolean;
+    subscribe:(topic:string, callback:(topic:string)=>string)=>string;
+    unsubscribe:(token:string)=>boolean;
 }
+export = PubSub;
