d7e23568b59e48df07098c2f55e4041da9ebb4d0
Anand Aravindan <anand.anand84@gmail.com>
2017-03-10 16:42:09 -0600

    Fix subscribe definition

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import { EventEmitter } from 'events';
 declare class PubSub extends EventEmitter{
     constructor();
     publish:(topic:string, data:any)=>boolean;
-    subscribe:(topic:string, callback:(topic:string)=>string)=>string;
+    subscribe:(topic:string, callback:(topic:string, data:any)=>void)=>string;
     unsubscribe:(token:string)=>boolean;
 }
 export = PubSub;
